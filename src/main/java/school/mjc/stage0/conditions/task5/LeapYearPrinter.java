c8f6fc647fa799fb95fbfbfe01bcd9dc2fe504d5
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-04 15:37:28 +0400

    task 4_5

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if(year % 4 != 0 || year == 1900)
+            System.out.println("not leap");
+        else
+            System.out.println("leap");
+
     }
 }
